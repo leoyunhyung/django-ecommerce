9397724a2d972ebe10f12d0f86c827d75b1a91d9
NemoMarket-Leo <97020138+leoyunhyung@users.noreply.github.com>
2022-07-12 12:07:55 +0900

    변경: ProductModelLike 외래키 related_name 변경

--- ecommerce/apps/likes/models.py
+++ ecommerce/apps/likes/models.py
@@ -10,11 +10,11 @@ class ProductModelLike(Model):
     user = models.ForeignKey('users.User',
                              verbose_name=_('유저'),
                              on_delete=models.CASCADE,
-                             related_name='likes')
+                             related_name='product_model_likes')
     product_model = models.ForeignKey('products.ProductModel',
                                       verbose_name=_('상품 모델'),
                                       on_delete=models.CASCADE,
-                                      related_name='likes')
+                                      related_name='product_model_likes')
 
     class Meta:
         verbose_name = verbose_name_plural = _('상품 모델 찜')
