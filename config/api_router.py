c1ca7d03e2beafd35d4039f141e599512ada453a
NemoMarket-Leo <97020138+leoyunhyung@users.noreply.github.com>
2022-07-12 16:17:05 +0900

    추가: Product api, nested router 연결

--- config/api_router.py
+++ config/api_router.py
@@ -5,13 +5,17 @@ from django.urls import include, path
 from rest_framework_nested import routers
 
 # Local
-from ecommerce.apps.products.api.views import ProductModelViewSet
+from ecommerce.apps.products.api.views import ProductModelViewSet, ProductViewSet
 
 router = routers.SimpleRouter(trailing_slash=False)
 
-router.register('product-models', ProductModelViewSet)
+router.register(r'product-models', ProductModelViewSet, basename='product-models')
+
+products_router = routers.NestedSimpleRouter(router, r'product-models', lookup='product_model')
+products_router.register(r'products', ProductViewSet, basename='product-model-products')
 
 app_name = 'api'
 urlpatterns = [
+                  path('', include(products_router.urls)),
                   path('', include("ecommerce.apps.users.urls")),
               ] + router.urls
