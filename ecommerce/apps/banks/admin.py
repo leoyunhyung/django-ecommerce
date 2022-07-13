02bf0941250d3ac979d40e1fcbd29a05cb90abfa
NemoMarket-Leo <97020138+leoyunhyung@users.noreply.github.com>
2022-07-13 18:33:53 +0900

    추가: 은행 계좌 어드민 사이트 구현

--- ecommerce/apps/banks/admin.py
+++ ecommerce/apps/banks/admin.py
@@ -1,3 +1,30 @@
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Local
+from ecommerce.apps.banks.models import BankAccount
+from ecommerce.apps.products.inlines import ProductOptionInline
+from ecommerce.apps.products.models import ProductModel, Product
+from ecommerce.bases.admin import Admin
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(Admin):
+    list_display = ('user', 'bank', 'account', 'name')
+    search_fields = ('user__name', 'bank', 'account', 'name')
+    ordering = ('-created',)
+    list_filter = ()
+
+    fieldsets = (
+        ("Fk", {"fields": ('user',)}),
+        ("Main", {"fields": ('id', 'bank', 'account', 'name')}),
+        ("Date", {"fields": ('created', 'modified',)}),
+    )
+
+    add_fieldsets = (
+        ("Fk", {"fields": ('user',)}),
+        ("Main", {"fields": ('id', 'bank', 'account', 'name')}),
+        ("Date", {"fields": ('created', 'modified',)}),
+    )
+
+    readonly_fields = ("created", "modified")
