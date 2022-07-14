054c9c8c3bbe38a0c3fdcd29791be700f68dc35e
NemoMarket-Leo <97020138+leoyunhyung@users.noreply.github.com>
2022-07-14 12:59:41 +0900

    추가: 클릭 어드민 사이트 구현

--- ecommerce/apps/clicks/admin.py
+++ ecommerce/apps/clicks/admin.py
@@ -1,3 +1,28 @@
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Local
+from ecommerce.apps.clicks.models import ProductModelClick
+from ecommerce.bases.admin import Admin
+
+
+@admin.register(ProductModelClick)
+class ProductModelClickAdmin(Admin):
+    list_display = ('user', 'product_model__name')
+    search_fields = ('user__email', 'product_model__name')
+    ordering = ('-created',)
+    list_filter = ()
+
+    fieldsets = (
+        ("Fk", {"fields": ('user',)}),
+        ("Main", {"fields": ('id', 'product_model')}),
+        ("Date", {"fields": ('created', 'modified',)}),
+    )
+
+    add_fieldsets = (
+        ("Fk", {"fields": ('user',)}),
+        ("Main", {"fields": ('product_model',)}),
+        ("Date", {"fields": ('created', 'modified',)}),
+    )
+
+    readonly_fields = ("created", "modified")
